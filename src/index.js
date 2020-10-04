d2ce944dd3beae66c83f2dcc1268ab0f789159e0
yuhei.kasai <yuhei@Air.local>
2020-10-05 02:38:22 +0900

    router の設定

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import createStore from "./reducks/store/store"
+import { ConnectedRouter } from "connected-react-router";
+import * as History from "history";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-export const store = createStore();
+const history = History.createBrowserHistory();
+export const store = createStore(history);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
 );
