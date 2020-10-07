5bb17efab2cf677171c7cded16eecbfeb4b2ccbf
yuhei.kasai <yuhei@Air.local>
2020-10-08 00:56:41 +0900

    selector の使用

--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { getUserId, getUsername } from "../reducks/users/selectors"
 
 const Home = () => {
+  const selector = useSelector(state => state);
+  const uid = getUserId(selector);
+  const username = getUsername(selector);
+
   return (
-    <h2>Home</h2>
+    <div>
+      <h2>Home</h2>
+      <p>uid:{uid}</p>
+      <p>username:{username}</p>
+    </div>
   )
 };
 
