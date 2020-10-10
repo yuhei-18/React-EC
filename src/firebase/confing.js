0ba15227c97b16064ade4da4cd8675801c5866a2
yuhei.kasai <yuhei@Air.local>
2020-10-10 22:23:37 +0900

    firebase の設定ファイルを作成

---
+++ src/firebase/confing.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyDScMtI9fl_ZOJxF_2961IskQi8EXae5KY",
+  authDomain: "react-ec-4dcf4.firebaseapp.com",
+  databaseURL: "https://react-ec-4dcf4.firebaseio.com",
+  projectId: "react-ec-4dcf4",
+  storageBucket: "react-ec-4dcf4.appspot.com",
+  messagingSenderId: "132602364850",
+  appId: "1:132602364850:web:7e78ba5b4fe126425f5aab",
+  measurementId: "G-288QGLDECD"
+};
